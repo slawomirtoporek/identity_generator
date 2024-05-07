0efd535b9d2ebefc05dafa3b07c9f85b8e2a6cdf
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-07 23:12:02 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const genders = ['Female', 'Male'];
+const femaleNames = ['Olivia', 'Amelia', 'Isla', 'Ava', 'Ivy', 'Freya', 'Lily', 'Florence', 'Mia', 'Willow'];
+const maleNames = ['Noah', 'Oliver', 'George', 'Arthur', 'Muhammad', 'Leo', 'Harry', 'Oscar', 'Archie', 'Henry'];
+const lastNames = ['García', 'Martin', 'Müller', 'Rodríguez', 'Fernández', 'John', 'Smith', 'Peter', 'Thomas', 'James'];
